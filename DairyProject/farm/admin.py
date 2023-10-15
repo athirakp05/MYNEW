e24a0951920f35d23417bb3a56b92e704396f131
Athira KP <athirakp2024a@mca.ajce.in>
2023-10-15 18:48:38 +0530

    commit3

--- DairyProject/farm/admin.py
+++ DairyProject/farm/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from .models import CustomUser
+from .models import Farmer , Society, IFSCcode
 
 
 # Register your models here.
 admin.site.register(CustomUser)
+admin.site.register(Farmer)
+admin.site.register(Society)
+admin.site.register(IFSCcode)
--- DairyProject/farm/models.py
+++ DairyProject/farm/models.py
@@ -13,6 +13,7 @@ class CustomUserManager(BaseUserManager):
         user.save(using=self._db)
         return user
 
+
     def create_superuser(self, email, password=None, role='Admin', phone=None, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
@@ -26,6 +27,7 @@ class CustomUserManager(BaseUserManager):
 
 
 class CustomUser(AbstractUser):
+
     CUSTOMER = 'Customer'
     SELLER = 'Seller'
     ADMIN = 'Admin'
@@ -35,14 +37,21 @@ class CustomUser(AbstractUser):
         (SELLER, 'Seller'),
         (ADMIN, 'Admin'),
     ]
+        
+    
+    
+    
 
     # Fields for custom user roles
     role = models.CharField(max_length=15, choices=ROLE_CHOICES, default=CUSTOMER)
     forget_password_token = models.UUIDField(null=True, blank=True)
     email = models.EmailField(unique=True)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
     objects = CustomUserManager()
-    username = models.CharField(max_length=150, unique=True)
-    phone = models.CharField(max_length=15, default='True')  # You can choose an appropriate default value
+    username= None
+    phone = models.CharField(max_length=15, null=True, blank=True)
     # Define boolean fields for specific roles (customize these as needed)
     is_customer = models.BooleanField(default=True)
     is_seller = models.BooleanField(default=False)
@@ -52,5 +61,36 @@ class CustomUser(AbstractUser):
     groups = models.ManyToManyField(CustomGroup, blank=True, related_name='custom_user_groups')
     user_permissions = models.ManyToManyField(Permission, blank=True, related_name='custom_user_permissions')
 
+
     def __str__(self):
         return self.email
+     
+class Society(models.Model):
+    district = models.CharField(max_length=20)
+    subdistrict = models.CharField(max_length=50)
+    panchayath = models.CharField(max_length=50)
+    ward_no = models.IntegerField()
+    farmer = models.ForeignKey('Farmer', on_delete=models.CASCADE, related_name='societies')
+
+class IFSCcode(models.Model):
+    bankname = models.CharField(max_length=50)
+    branch = models.CharField(max_length=50)
+    accno = models.IntegerField()
+    farmer = models.ForeignKey('Farmer', on_delete=models.CASCADE, related_name='ifsc_codes')
+
+class Farmer(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    house_name = models.CharField(max_length=200)
+    city = models.CharField(max_length=50)
+    pin_code = models.IntegerField()
+    occupation = models.CharField(max_length=20)
+    gender = models.CharField(max_length=10)
+    dob = models.DateField()
+    rationcard_no = models.IntegerField()
+    email = models.EmailField()
+    mobile = models.DecimalField(max_digits=10, decimal_places=0)
+    acc_no = models.IntegerField()
+    society = models.ForeignKey(Society, on_delete=models.CASCADE, related_name='farmers')
+
+    # Add any other fields you need for your Farmer model
